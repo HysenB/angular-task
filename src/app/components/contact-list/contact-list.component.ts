1511002c6416da09d44db1ceff784a96608b9178
HysenB <hysi2000@live.com>
2022-08-23 01:52:10 +0200

    bug-fix

--- src/app/components/contact-list/contact-list.component.ts
+++ src/app/components/contact-list/contact-list.component.ts
@@ -13,44 +13,34 @@ export class ContactListComponent implements OnInit {
   contacts!: Contact[];
   contact: Contact = new Contact();
   name!: string;
-  contactId!: number;
+  errorMessage?: string;
+  countId!: number;
 
   constructor(
     private contactService: ContactService,
     private router: Router,
   ) {
-    this.getContacts();
-
-  }
-
-
-
-  private getContacts() {
     this.contactService.getAllContacts().subscribe((contact) => {
       this.contacts = contact;
+      this.countId++;
     });
   }
 
   createSubmit() {
-    // this.contactId = this.contacts.length;
-    this.getContacts();
-    this.contact.id = this.contacts.length + 1;
-    if(this.name){
+    this.contact.id = this.countId;
+    if (this.name) {
       this.contact.first_name = this.name;
       this.contact.first_name = this.name.trim().split(' ')[0];
       this.contact.last_name = this.name.trim().substring(this.contact.first_name.length + 1);
+    } else {
+      this.errorMessage = 'Name is invalid';
     }
 
-
-    // console.log(this.contact.id);
-    // console.log(this.contact.first_name);
-    // console.log(this.contact.last_name);
-    if(this.name && this.name.trim().length >= 1){
+    if (this.name && this.name.trim().length >= 1) {
       this.contactService.createContact(this.contact).subscribe(data => {
         this.router.navigateByUrl('/contacts');
-        // this.router.navigate(['/contacts']).then();
       });
-      this.getContacts();
+      window.location.reload();
     }
   }
 
--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -22,7 +22,9 @@ export class ContactComponent implements OnInit {
   countries!: Country[];
   vAddress: Address = new Address();
   showForm: boolean = false;
-  addressId!: Address[];
+  // addressId!: Address[];
+  countAddressId!: number;
+  errorMessage?: string;
 
   newForm = new FormGroup({
     street1: new FormControl('', Validators.required),
@@ -39,68 +41,55 @@ export class ContactComponent implements OnInit {
     private countryService: CountryService,
     private router: Router
   ) {
-      this.getAllAC();
-    }
-
-
-  getAllAC(){
     this.activatedRoute.params.subscribe((params) => {
-      if(params['id']){
+      if (params['id']) {
         this.contactService.getContact(params['id']).subscribe(contact => this.contact = contact);
-        this.addressService.getAddress(params['id']).subscribe(address => this.addresses = address);
+        this.addressService.getAddress(params['id']).subscribe(address => {
+          this.addresses = address
+        });
+        this.addressService.getAllAddresses().subscribe(() => {
+          this.countAddressId++;
+        })
       }
-
     })
-
     this.countryService.getAllCountries().subscribe(country => this.countries = country);
-
   }
 
+
   addForm(): void {
+    this.newForm.reset();
     this.newForm.markAsPristine();
     this.newForm.markAsUntouched();
-    this.newForm.reset();
     this.showForm = !this.showForm;
   }
 
   submit(): void {
-    window.location.reload();
-    this.getAllAC();
-    this.addressService.getAllAddresses().subscribe(value => {
-      this.addressId = value;
-    });
-    if(this.addressId){
-      console.log("addressId: " + this.addressId.length);
-      this.vAddress.id = ++this.addressId.length;
-      console.log("addressId: " + this.addressId.length);
-    }
+    this.vAddress.id = this.countAddressId;
     this.vAddress.contactId = this.contact.id.toString();
-    if(this.newForm.valid){
+    if (this.newForm.valid && !this.newForm.validator) {
       this.vAddress.street1 = this.newForm.value.street1;
       this.vAddress.street2 = this.newForm.value.street2;
       this.vAddress.town = this.newForm.value.town;
       this.vAddress.country = this.newForm.value.country;
 
       this.addressService.createAddress(this.vAddress).subscribe(() => {
-        // this.router.navigateByUrl(`/contacts/${this.vAddress.contactId}`)
-
+        this.router.navigateByUrl(`/contacts/${this.vAddress.contactId}`)
       })
 
+      this.errorMessage = '';
+      this.addForm();
+      window.location.reload();
+    } else if (this.showForm) {
+      this.errorMessage = 'Blank inputs!';
+    } else {
+      this.addresses.map(address => {
+        let id: number = address.id;
+        this.addressService.updateAddress(address, id).subscribe(() => {
+          this.router.navigateByUrl(`/contacts/${address.contactId}`);
+        })
+      })
+      window.location.reload();
     }
-
-    this.getAllAC();
-    this.addresses.map(address => {
-      let id:number = address.id;
-      this.addressService.updateAddress(address, id).subscribe(() => {
-        // this.router.navigateByUrl(`/contacts/${address.contactId}`);
-      })
-    })
-    this.addForm();
-    // window.location.reload();
-    this.newForm.markAsPristine();
-    this.newForm.markAsUntouched();
-    this.newForm.reset();
-    this.getAllAC();
 
 
   }
@@ -109,6 +98,7 @@ export class ContactComponent implements OnInit {
 
 
   }
+
 
 }
 
